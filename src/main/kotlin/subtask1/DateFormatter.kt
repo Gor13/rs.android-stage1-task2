9872658e38211074e72e7ff48e7bc6cb11182123
Hardzei <pavelko1331@gmail.com>
2020-04-09 14:16:29 +0300

    completed task2.1

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,50 @@
 package subtask1
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
+
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+
+        val months = arrayOf(
+            "января", "февраля", "марта", "апреля", "мая", "июня",
+            "июля", "августа", "сентября", "октября", "ноября", "декабря"
+        )
+        val weekday =
+            arrayOf(
+                "понедельник",
+                "вторник",
+                "среда",
+                "четверг",
+                "пятница",
+                "суббота",
+                "воскресенье"
+            )
+
+        var result = "Такого дня не существует"
+        try {
+
+            var date = LocalDate.parse(
+                "${year}-${if (month.length == 1) {
+                    "0$month"
+                } else {
+                    month
+                }}-${if (day.length == 1) {
+                    "0$day"
+                } else {
+                    day
+                }}"
+            )
+
+            result =
+                "${date.dayOfMonth} ${months[date.month.value - 1]}, ${weekday[date.dayOfWeek.value - 1]}"
+        } catch (ex: Exception) {
+            return result
+        }
+        return result
     }
 }
