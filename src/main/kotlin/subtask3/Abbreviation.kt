401d3f4526a6f6ad2037ff151292b302cbd28f69
Hardzei <pavelko1331@gmail.com>
2020-04-09 15:43:01 +0300

    completed task2.3

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -2,8 +2,23 @@ package subtask3
 
 class Abbreviation {
 
-    // TODO: Complete the following function
+
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+        var result = "NO"
+        var ourString = ""
+        for (indexA in 0..a.length -1){
+            for (indexB in 0..b.length -1){
+                if (a.toUpperCase().get(indexA) == b.get(indexB)){
+                    ourString = "${ourString}${a.toUpperCase().get(indexA)}"
+                }
+            }
+        }
+
+        if (ourString == b){
+            result = "YES"
+        }
+
+        return result
     }
 }
