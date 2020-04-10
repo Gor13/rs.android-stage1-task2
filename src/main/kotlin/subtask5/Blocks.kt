0f11886465767ac7cc6cc1d48970efe1e88be00d
Hardzei <pavelko1331@gmail.com>
2020-04-10 17:14:30 +0300

    completed task2.6

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -2,10 +2,18 @@ package subtask5
 
 import kotlin.reflect.KClass
 
+
+
 class Blocks {
 
-    // TODO: Complete the following function
-    fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+
+    fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
+        //  throw NotImplementedError("Not implemented")
+        lateinit var result: Any
+
+
+
+
+        return result
     }
 }
--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,19 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        // throw NotImplementedError("Not implemented")
+        var fnNull = 0
+        var fnPlusOne = 1
+        while (true) {
+            if (fnNull * fnPlusOne == n) {
+                return mutableListOf<Int>(fnNull, fnPlusOne, 1).toIntArray()
+            } else if (fnNull * fnPlusOne > n) {
+                return mutableListOf<Int>(fnNull, fnPlusOne, 0).toIntArray()
+            } else {
+                val s = fnPlusOne
+                fnPlusOne = fnPlusOne + fnNull
+                fnNull = s
+            }
+        }
     }
 }
