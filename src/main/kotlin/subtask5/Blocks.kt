eaa935d56700a26301c34ff43cb17679557b0df9
Hardzei <pavelko1331@gmail.com>
2020-04-10 18:51:42 +0300

    completed task2.5

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,5 +1,9 @@
 package subtask5
 
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 import kotlin.reflect.KClass
 
 
@@ -9,11 +13,42 @@ class Blocks {
 
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
         //  throw NotImplementedError("Not implemented")
-        lateinit var result: Any
+        var resultStr = ""
+        var resultInt = 0
+        var resultDate = ""
+        var resultLong: Long = 0
 
+        if ((blockB.java).toString() == "class java.lang.String"){
+            for (element in blockA){
+                if (element is String){
+                    resultStr = resultStr + element
+                }
+            }
+            return resultStr
+        }
 
+        if ((blockB.java).toString() == "int"){
+            for (element in blockA){
+                if (element is Int){
+                    resultInt += element
+                }
+            }
+            return resultInt
+        }
 
+        if ((blockB.java).toString() == "class java.time.LocalDate"){
+            for (element in blockA){
+                if (element is LocalDate){
+                    if (resultLong < SimpleDateFormat("yyyy-MM-dd").parse(element.toString()).time){
+                        resultLong = SimpleDateFormat("yyyy-MM-dd").parse(element.toString()).time
+                        resultDate = element.format( DateTimeFormatter.ofPattern("dd.MM.yyyy"))
 
-        return result
+                    }
+                }
+            }
+            return resultDate
+        }
+
+        return Any()
     }
 }
